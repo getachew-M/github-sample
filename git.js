cf1f9d6629942cf357bd9a78d2edd8c7c1ef1e16
getachew <gvhvhvhv@yahoo.com>
2019-10-14 19:47:04 -0400

    first commit

--- git.js
+++ git.js
@@ -1,1 +1,10 @@
+var exam=prompt("enter ur result");
+if(exam>=50){
+console.log("congratulation u passed next");
+}
+else{
+    console.log("u faild can u try again exam please")
+}
+
+
 console.log("welcome to script language");
